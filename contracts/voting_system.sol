1ceb650bd407a4206ebf55bd7ea9a12b3f816922
Kalicharan-Prajapati <kalicharan@geeksofgurukul.com>
2025-10-05 16:06:16 +0530

    Update voting_system.sol

--- contracts/voting_system.sol
+++ contracts/voting_system.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier:
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract GroupVotingSystem {
@@ -389,5 +389,6 @@ contract GroupVotingSystem {
             : p.votingDeadline - block.timestamp;
     }
 }
+
 
 
